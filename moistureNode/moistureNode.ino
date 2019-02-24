0dadc2318a4d84ad97f796add26d29b0c2b8d28d
Mathew Varghese <mathewvarghesemanu@gmail.com>
2019-02-24 21:22:40 +0530

    v3
    moisturenode completed

--- moistureNode/moistureNode.ino
+++ moistureNode/moistureNode.ino
@@ -7,31 +7,52 @@
 
 #define SSID "Manu"
 #define SSID_PASSWORD "ambalathil1234"
-//D5 RELAY D6 LED A0 ANALOG 
+//D5 RELAY D6 LED A0 ANALOG
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
-int moistureValue=0,moisturePin=A0,motorPin=D5;
+
+int moisturePin = A0, motorPin = D5, ledPin = D6;
+int moistureValue = 0, threshold = 100;
+
 void setup() {
   Serial.begin(115200);
-  
+
   pinMode(moisturePin, INPUT);
   pinMode(motorPin, OUTPUT);
-
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(motorPin, LOW);
+digitalWrite(ledPin, LOW);
   thing.add_wifi(SSID, SSID_PASSWORD);
 
   // digital pin control example (i.e. turning on/off a light, a relay, configuring a parameter, etc)
-  thing["motor"] << digitalPin(motorPin);
+  thing["motorcontrol"] << digitalPin(motorPin);
 
+  thing["ledcontrol"] << digitalPin(ledPin);
   // resource output example (i.e. reading a sensor value)
-   thing["millis"] >> outputValue(millis());
+  thing["millis"] >> outputValue(millis());
 
-   thing["moistureValue"] >> outputValue(analogRead(moisturePin));
+  thing["moistureValue"] >> outputValue(moistureValue=analogRead(moisturePin));
 
-   //thing["threshold"] >> outputValue();
+  thing["threshold"] << inputValue(threshold);
+ /* thing["resource"] = []() {
+    if (analogRead(moisturePin) < threshold)
+    {
+      digitalWrite(ledPin, LOW);
+      Serial.println("hey");}
+    else
+      digitalWrite(ledPin, HIGH);
+  };
+*/
+
   // more details at http://docs.thinger.io/arduino/
 }
 
 void loop() {
   thing.handle();
-  
-
+  Serial.println(threshold);
+if (moistureValue < threshold)
+    {
+      digitalWrite(motorPin, HIGH);
+      Serial.println("hey");}
+    else
+      digitalWrite(motorPin, LOW); 
 }
